2889a0ff2c88bf2815f84d3ff18b576daf5a8773
Aydin <aydin-cakmak45@hotmail.com>
2024-01-03 20:31:56 +0100

    Local1 and Local2 uploaded

---
+++ src/Local1.java
@@ -0,0 +1,8 @@
+public class Local1 {
+
+    public static void main(String[] args) {
+        System.out.println("Git test");
+        System.out.println("Yeni satir eklendi");
+        System.out.println("yepyeni");
+    }
+}
