2a67d79ce508a358a0b21a31d041a57efe4071e7
Aydin Cakmak <aydin-cakmak45@hotmail.com>
2024-01-05 20:25:11 +0100

    Local1 eski dostlar guncellendi

--- src/Local1.java
+++ src/Local1.java
@@ -1,7 +1,7 @@
 public class Local1 {
 
     public static void main(String[] args) {
-        System.out.println("Git test");
+        System.out.println("BU TESTI AYDIN BRANCH DA DEGSITIRDIM");
         System.out.println("Yeni satir eklendi");
         System.out.println("yepyeni");
     }
