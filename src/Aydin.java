8de06d876a58ccaa5cd28cf1f843db0651c2e5b8
Aydin Cakmak <aydin-cakmak45@hotmail.com>
2024-01-05 19:27:04 +0100

    Yeni dosyalar eklendi

---
+++ src/Aydin.java
@@ -0,0 +1,6 @@
+public class Aydin {
+    public static void main(String[] args) {
+        System.out.println("Aydinndosya eklendi");
+        System.out.println("Yeni dosya eklendi");
+    }
+}
--- src/Local2.java
+++ src/Local2.java
@@ -2,6 +2,7 @@ public class Local2 {
     public static void main(String[] args) {
         System.out.println("Test1");
         System.out.println("Test2");
+        System.out.println("Yeni dosya");
 
     }
 }
