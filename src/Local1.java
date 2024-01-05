c7570a0e6b10f8316adb2753bd4a0b861bd36abf
Aydin Cakmak <aydin-cakmak45@hotmail.com>
2024-01-05 20:50:26 +0100

    istenilen Degisiklikler yapildi

--- src/Local1.java
+++ src/Local1.java
@@ -2,7 +2,7 @@ public class Local1 {
 
     public static void main(String[] args) {
         System.out.println("BU TESTI AYDIN BRANCH DA DEGSITIRDIM");
-        System.out.println("Yeni satir eklendi");
+        System.out.println("BU SATIRDA DEGISIKLIK YAPILDI :::::Yeni satir eklendi");
         System.out.println("yepyeni");
     }
 }
